ab28119e2c3eea6000d32b10051931712f1fa678
kjdhl <90534103+kjdhl@users.noreply.github.com>
2022-08-06 15:26:16 -0300

    Add files via upload

--- Atividade6M4.java
+++ Atividade6M4.java
@@ -1,36 +1,19 @@
-import java.io.*;
-import java.io.Serializable;
-public class Aluno implements java.io.Serializable {
-	public String nome;
-	public String classe;
 
- }
- public class serializando {
-
-	public static void main(String [] args) {
-		Aluno e  = new Aluno();
-		e.nome = "aluno1";
-		e.classe = "A";
-	   
-	   
-	
-		FileOutputStream objeto = new FileOutputStream("C:/alunos.ser");
-		ObjectOutputStream out = new ObjectOutputStream(objeto);
-		out.writeObject(e);
-		out.close();
-		objeto.close();
-	   
-	}
- }
-
- public class deserealizando{
-
-	public static void main(String [] args) {
-		   FileInputStream objeto = new FileInputStream("C:/alunos.ser");
-		   ObjectInputStream out = new ObjectInputStream(objeto);
-		   e = (Aluno) out.readObject();
-		   out.close();
-
-	}
-
- }
+public class main {
+    public static void main(String args[]) {
+      int a = 10;
+      int b = 0;
+      try{
+        System.out.println("a = "  + a + ",b =" + b );  
+        System.out.println("a/b = " + a/b);
+      }catch(ArithmeticException e){
+          System.out.println("Divisão por 0 invalida!");
+          System.out.println("Para b = 1 ");
+          b = 1;
+          System.out.println("a/b = " + a/b);
+          
+      }
+      
+      
+    }
+}
